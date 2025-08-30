3a0987529d4042a1b4f4fbf2587cbecfeafe5323
Prakhar Srivastava <Srivprak0106@gmail.com>
2025-08-31 01:34:39 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,5 +5,5 @@ resource aws_s3_bucket my_bucket {
         bucket = "my-1st-bucket-via-terraform"    
 }
 
-Proof of bucket creation is :- 
+Proof of bucket creation is :- an image attcahed to the repo by name image.png whose path is :- image.png
 
